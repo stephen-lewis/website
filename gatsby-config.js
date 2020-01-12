70c458c9c00ea1712c5d163933555b8cefb276ef
Stephen Lewis <mail@stephen-lewis.me.uk>
2020-01-12 00:05:49 +0000

    Added various gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://stephen-lewis.me.uk"
+  },
   plugins: [
-    'gatsby-plugin-eslint',
-    'gatsby-plugin-material-ui',
-    'gatsby-plugin-react-helmet',
-  ],
+    "gatsby-plugin-eslint",
+    "gatsby-plugin-material-ui",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-manifest",
+    "gatsby-plugin-offline"
+  ]
 };
