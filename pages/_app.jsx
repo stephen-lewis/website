f56907ac7169fdd3ec389ce88faf43aff27e8ebe
Stephen Lewis <mail@stephen-lewis.me.uk>
2020-01-20 22:22:32 +0000

    Rearrange file structure to suit NextJS

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import 'typeface-open-sans';
+
+import '../styles/reset.css';
+import '../styles/layout.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import About from '../components/About';
+import Experience from '../components/Experience';
+import Footer from '../components/Footer';
+import Intro from '../components/Intro';
+import Menu from '../components/Menu';
+import Skills from '../components/Skills';
+import Social from '../components/Social';
+
+const Index = () => (
+  <>
+    <Menu />
+    <Social />
+    <Intro />
+    <About />
+    <Skills />
+    <Experience />
+    <Footer />
+  </>
+);
+
+export default Index;
