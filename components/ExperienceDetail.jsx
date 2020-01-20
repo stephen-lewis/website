29199a7b4e8e69d10a3bb3ed551df199729f8af4
Stephen Lewis <mail@stephen-lewis.me.uk>
2020-01-20 22:46:31 +0000

    [WIP] Laid out experience components.

---
+++ components/ExperienceDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExperienceDetail = ({ children }) => (
+  <div className="job-detail">
+    {children}
+  </div>
+);
+
+export default ExperienceDetail;
---
+++ components/ExperienceList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExperienceList = ({ children }) => (
+  <div className="job-list-container">
+    {children}
+  </div>
+);
+
+export default ExperienceList;
